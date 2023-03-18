9d870d53fcc9cfcef7af6a3899965e66db8c4251
wjm <wangjm@hrtps.com>
2023-03-18 16:53:23 +0800

    fix: 依赖错误

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,9 @@ import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
 
 async function bootstrap() {
-  dotenv.config({ path: '.env.local' }); // Load .env.local file
+  if (process.env.NODE_ENV === 'development') {
+    dotenv.config({ path: '.env.local' });
+  } // Load .env.local file
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   await app.listen(3000);
